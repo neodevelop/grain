6acc641c8e9bcc64169c513fca7215a1fcb28e08
neodevelop <neodevelop@gmail.com>
2011-04-18 15:42:04 -0500

    WIP para #7 Se han modificado los plugins y solo dejamos los necesarios de momento...

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,4 @@
-grails.project.plugins.dir = "${userHome}/.grails/1.3.6/grain/plugins"
+grails.project.plugins.dir = "${userHome}/.grails/1.3.7/grain/plugins"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -19,18 +19,17 @@ grails.project.dependency.resolution = {
 
   plugins {
     runtime 'org.grails.plugins:spring-security-core:1.0.1'
-    runtime 'org.grails.plugins:tiny-mce:0.1.3'
     runtime 'org.grails.plugins:mail:0.9'
     runtime 'org.grails.plugins:settings:1.4'
     runtime 'org.grails.plugins:jquery:1.4.2.7'
-	runtime 'org.grails.plugins:jquery-validation:1.7'
-    runtime 'org.grails.plugins:jquery-validation-ui:1.1'
-    runtime 'org.grails.plugins:constraints:0.5.1'
-    runtime 'org.grails.plugins:perf4j:0.1.1'
+	  //runtime 'org.grails.plugins:jquery-validation:1.7'
+    //runtime 'org.grails.plugins:jquery-validation-ui:1.1'
+    //runtime 'org.grails.plugins:constraints:0.5.1'
+    //runtime 'org.grails.plugins:perf4j:0.1.1'
 
-    test 'org.grails.plugins:code-coverage:1.2'
-    test 'org.grails.plugins:codenarc:0.8.1'
-    test 'org.grails.plugins:gmetrics:0.3'
+    //test 'org.grails.plugins:code-coverage:1.2'
+    //test 'org.grails.plugins:codenarc:0.8.1'
+    //test 'org.grails.plugins:gmetrics:0.3'
     //test 'org.grails.plugins:geb:0.5.1'
     //test 'org.grails.plugins:spock:0.5-groovy-1.7'
   }
