5a4c6fd4db62cc48969b1464d9cb670529f330d4
neodevelop <neodevelop@gmail.com>
2011-05-24 22:05:51 -0500

    Cambiamos el nombre del archivo generado para el despliegue en raiz

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,7 +3,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
-grails.project.war.file = "target/ROOT.war"
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
   inherits("global") {
