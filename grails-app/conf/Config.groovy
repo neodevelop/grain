5cbb5995d45c9acf02d42b463ce720cc2b06705d
neodevelop <neodevelop@gmail.com>
2011-08-06 19:39:47 -0500

    Quitamos el password que no usamos

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -194,7 +194,7 @@ grails {
     host = "smtp.gmail.com"
     port = 465
     username = "cursos@synergyj.com"
-    password = "entrenamiento"
+    password = ""
     props = ["mail.smtp.auth":"true",
         "mail.smtp.socketFactory.port":"465",
         "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
