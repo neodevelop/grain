0eee7b43fcb98b92b8b0095639def900eae36f42
neodevelop <neodevelop@gmail.com>
2011-09-22 23:27:47 -0500

    WIP #123 Generamos el String para la vista de prueba

--- grails-app/services/com/synergyj/grain/event/QueueForMailService.groovy
+++ grails-app/services/com/synergyj/grain/event/QueueForMailService.groovy
@@ -16,23 +16,77 @@
 package com.synergyj.grain.event
 
 import grails.util.Environment
+import org.codehaus.groovy.grails.commons.GrailsResourceUtils
+import org.codehaus.groovy.grails.web.servlet.DefaultGrailsApplicationAttributes
+import grails.util.GrailsWebUtil
+import org.springframework.web.context.support.WebApplicationContextUtils
+import org.codehaus.groovy.grails.web.context.ServletContextHolder
+import org.springframework.web.context.request.RequestContextHolder
+import javax.servlet.http.HttpServletRequest
 
 class QueueForMailService {
-  
+
   static transactional = true
 
   static rabbitQueue = NotificationService.MAIL_QUEUE_NOTIFICATION
 
   static throwExceptions = false
 
+  static PATH_TO_MAILVIEWS = "/WEB-INF/grails-app/views"
+
   def mailService
 
-  void handleMessage(Map message){
-    switch(Environment.current){
+  def groovyPagesTemplateEngine
+
+  void handleMessage(Map message) {
+    switch (Environment.current) {
       case Environment.DEVELOPMENT:
-        // Que huevos!!!
-        Thread.sleep(5000)
-        log.debug("${Environment.current} - ${message}")
+        try {
+
+          def engine = groovyPagesTemplateEngine
+          def templateName = message.view
+          def requestAttributes = RequestContextHolder.getRequestAttributes()
+          boolean unbindRequest = false
+
+          // outside of an executing request, establish a mock version
+          if (!requestAttributes) {
+            def servletContext = ServletContextHolder.getServletContext()
+            def applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext)
+            requestAttributes = GrailsWebUtil.bindMockWebRequest(applicationContext)
+            unbindRequest = true
+          }
+          def servletContext = requestAttributes.request.servletContext
+          def request = requestAttributes.request
+
+          def grailsAttributes = new DefaultGrailsApplicationAttributes(servletContext);
+          // See if the application has the view for it
+          def uri = getMailViewUri(templateName, request)
+
+          def r = engine.getResourceForUri(uri)
+          def t = engine.createTemplate(r)
+
+          def out = new StringWriter();
+          def originalOut = requestAttributes.getOut()
+          requestAttributes.setOut(out)
+          try {
+            if (message.model instanceof Map) {
+              t.make(message.model).writeTo(out)
+            }
+            else {
+              t.make().writeTo(out)
+            }
+          }
+          finally {
+            requestAttributes.setOut(originalOut)
+            if (unbindRequest) {
+              RequestContextHolder.setRequestAttributes(null)
+            }
+          }
+          println out
+          log.debug("${Environment.current} - ${message}")
+        } catch (Exception e) {
+          println e.message
+        }
         break
       case Environment.TEST:
         log.debug("${Environment.current} - ${message}")
@@ -42,9 +96,34 @@ class QueueForMailService {
           to message.to
           from message.from
           subject message.subject
-          body(view:message.view,model:message.model)
+          body(view: message.view, model: message.model)
         }
         break
     }
   }
+
+  protected String getMailViewUri(String viewName, HttpServletRequest request) {
+
+    def buf = new StringBuilder(PATH_TO_MAILVIEWS)
+
+    if (viewName.startsWith("/")) {
+      def tmp = viewName[1..-1]
+      if (tmp.indexOf('/') > -1) {
+        def i = tmp.lastIndexOf('/')
+        buf << "/${tmp[0..i]}/${tmp[(i + 1)..-1]}"
+      }
+      else {
+        buf << "/${viewName[1..-1]}"
+      }
+    }
+    else {
+      if (!request) throw new IllegalArgumentException(
+        "Mail views cannot be loaded from relative view paths where there is no current HTTP request")
+      def grailsAttributes = new DefaultGrailsApplicationAttributes(request.servletContext)
+      buf << "${grailsAttributes.getControllerUri(request)}/${viewName}"
+
+    }
+    return buf.append(".gsp").toString()
+  }
+
 }
