68eb18ea8e3422bfba6e1046f7f2789f575a3f66
neodevelop <neodevelop@gmail.com>
2011-10-12 22:01:56 -0500

    WIP #154 hacemos debut para ver en que parte se queda, parece que DM cambio el formato de envío de parámetros en la fecha y en los separadores de decimales en montos

--- grails-app/services/com/synergyj/grain/event/DineroMAilService.groovy
+++ grails-app/services/com/synergyj/grain/event/DineroMAilService.groovy
@@ -19,6 +19,7 @@ import groovy.xml.MarkupBuilder
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.XML
 import static groovyx.net.http.Method.GET
+import static groovyx.net.http.Method.POST
 import com.synergyj.grain.course.Payment
 import java.text.SimpleDateFormat
 import com.synergyj.grain.course.PaymentStatus
@@ -27,7 +28,7 @@ class DineroMailService {
 
   static transactional = true
 
-  static urlIPN = 'https://mexico.dineromail.com/Vender/Consulta_IPN.asp'
+  static urlIPN = "https://mexico.dineromail.com/Vender/Consulta_IPN.asp"
   static ipnPassword = ''
   static accountNumber = ''
   static timeoutDM = 1000 * 60 * 5
@@ -68,14 +69,12 @@ class DineroMailService {
       // Obtenemos el pago que vamos a consultar por su id de TX
       payment = Payment.findByTransactionId(txId)
     }
-
     // Generamos el cuerpo de la petición con el id de TX
     def requestData = prepareXml(payment.transactionId)
     // Creamos un objeto HTTPBuilder con la url de DM - IPN
     def http = new HTTPBuilder(urlIPN)
-
     // Ejecutamos la petición con el documento
-    http.request(GET, XML) { req ->
+    http.request(POST, XML) { req ->
       req.getParams().setParameter("http.connection.timeout", new Integer(timeoutDM))
       req.getParams().setParameter("http.socket.timeout", new Integer(timeoutDM))
       // DM solicita que el XML sea enviado en el parámetro DATA
@@ -83,11 +82,13 @@ class DineroMailService {
 
       // Manejamos la respuesta satisfactoria
       response.success = {resp, data ->
+        log.debug("Manejando la respuesta de la petición")
         isPayed = handleResponse(resp, data, payment)
       }
 
       // Manejamos la respuesta en errores
       response.failure = { resp ->
+        log.error("Error en la petición para la tx:${payment.transactionId} ")
         throw new RuntimeException("Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}")
       }
     }
@@ -125,19 +126,24 @@ class DineroMailService {
       // Iteramos las operaciones obtenidas de esta transacción
       data.DETALLE.OPERACIONES.OPERACION.each {operation ->
         def tx = new Expando()
-        // Obtenemos el ID de la TX
-        tx.id = operation.ID.text()
-        // Obtenemos el status
-        tx.statusOperation = statusOperation["${operation.ESTADO.text() ?: '0'}"]
-        // Obtenemos la fecha
-        def sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa")
-        tx.operationDate = sdf.parse(operation.FECHA.text())
-        // Obtenemos el monto
-        tx.amount = new BigDecimal(operation.MONTO.text())
-        // Obtenemos el monto total
-        tx.totalAmount = new BigDecimal(operation.MONTONETO.text())
-        // Obtenemos el método de pago
-        tx.paymentMethod = paymentMethods["${operation.METODOPAGO.text() ?: '0'}"]
+        log.debug "Obtenemos el ID de la TX"
+        tx.id = operation.ID?.text()
+        log.debug "Obtenemos el status"
+        tx.statusOperation = statusOperation["${operation.ESTADO?.text() ?: '0'}"]
+        try{
+          log.debug "Obtenemos la fecha y parseamos ${operation.FECHA}"
+          def sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss aa")
+          def fecha = operation.FECHA?.text() - '.'
+          log.debug fecha
+          tx.operationDate = sdf.parse(fecha.toUpperCase())
+        }catch(Exception e){ log.error e.message }
+        log.debug "Fecha en objeto parseada: ${tx.operationDate}"
+        log.debug "Obtenemos el monto"
+        tx.amount = new BigDecimal(operation.MONTO?.text())
+        log.debug "Obtenemos el monto total $operation.MONTONETO"
+        tx.totalAmount = new BigDecimal(operation.MONTONETO?.text().replace(',','.'))
+        log.debug "Obtenemos el método de pago"
+        tx.paymentMethod = paymentMethods["${operation.METODOPAGO?.text() ?: '0'}"]
         operationsForTransaction << tx
       }
       // Buscamos si entre las operaciones existe el pago
