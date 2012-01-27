0f69602d08dd3f80b5ea3d1c664d07d2c448fba7
neodevelop <neodevelop@gmail.com>
2012-01-27 15:00:57 -0600

    Creando prueba
    -Agregamos la prueba de unidad para el método que agrega un usuario a un curso y crea un registro

--- test/unit/com/synergyj/grain/course/RegistrationServiceTests.groovy
+++ test/unit/com/synergyj/grain/course/RegistrationServiceTests.groovy
@@ -2,22 +2,29 @@ package com.synergyj.grain.course
 
 import grails.test.*
 import com.synergyj.grain.auth.User
+import com.synergyj.grain.event.NotificationService
 
 class RegistrationServiceTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
 
-    protected void tearDown() {
-        super.tearDown()
-    }
-
-    void testAddUserToScheduledCourse() {
-      def user = new User(email:'user@user.com',password:password)
-      mockDomain(User,[user])
-      //def scheduledCourse = new ScheduledCourse(id:1,costByCourse:6500,costByModule:2000)
-      //mockDomain(ScheduledCourse,[scheduledCourse])
-      // TODO: probar los casos
-      // Muchos métodos estáticos en el servicio, tal vez lo mejor sea hacer una prueba de integración...
-    }
+  void testAddUserToScheduledCourse() {
+    // Intanciamos el servicio a probar
+    def registrationService = new RegistrationService()
+    //Creamos la instancia Mock del usuario
+    def existingUser = new User(email: "something@server.com", password: "secret")
+    mockDomain(User,[existingUser])
+    // Creamos la instancia Mock del Curso al que queremos registrar al usuario
+    def scheduledCourse = new ScheduledCourse(id:1L)
+    mockDomain(ScheduledCourse,[scheduledCourse])
+    // Creamos el mock para los objetos de Registro
+    mockDomain(Registration)
+    // Creamos el colaborador del servicio prinicipal
+    def notificationServiceMock = mockFor(NotificationService)
+    notificationServiceMock.demand.sendCourseRegistration(1..1) { Long id -> return 0 }
+    // Asignamos el colaborador al servicio principal
+    registrationService.notificationService = notificationServiceMock.createMock()
+    // Probamos el servicio
+    def registration = registrationService.addUserToScheduledCourse(existingUser,1L)
+    // Corroboramos que se ha creado el registro
+    assertNotNull(registration)
+  }
 }
