b72548398400183c081f4ef947c7aa7230fa2bdf
neodevelop <neodevelop@gmail.com>
2012-03-08 14:03:53 -0600

    Agregando el repo temporal para jRobin que usa Java Melody

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,6 +10,7 @@ grails.project.dependency.resolution = {
   }
   log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
   repositories {
+    mavenRepo name: "OpenNMS", root: "http://repo.opennms.org/content/groups/opennms.org-release"
     grailsPlugins()
     grailsHome()
     grailsCentral()
