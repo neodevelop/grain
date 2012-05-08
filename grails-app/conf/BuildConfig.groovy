ba80c41690fdbeaacfcf1d24e0b85580ae6df4f9
neodevelop <neodevelop@gmail.com>
2012-05-08 13:03:43 -0500

    Arreglando las versiones de los plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,8 +38,8 @@ grails.project.dependency.resolution = {
     runtime 'org.grails.plugins:google-analytics:1.0'
     runtime 'org.grails.plugins:jasper:1.2.1'
     runtime 'org.grails.plugins:rabbitmq:0.3.2'
-    compile ":quartz:1.0-RC1"
-    compile ':grails-melody:1.13'
+    compile ":quartz:0.4.2"
+    runtime ':grails-melody:1.7'
     runtime ':amazon-s3:0.8.2'
     //runtime 'org.grails.plugins:jquery-validation:1.7'
     //runtime 'org.grails.plugins:jquery-validation-ui:1.1'
