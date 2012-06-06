87cd477350da571eba9cd38de250e30b4ab81675
neodevelop <neodevelop@gmail.com>
2012-06-05 23:55:21 -0500

    Ajustando el css que carga el bootstrap

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,9 @@ modules = {
   home {
     resource url:'/css/grain.css'
   }
+  overrides { // Arreglando bootstra-fixtaglib.css
+    'bootstrap-fixtaglib' {
+      defaultBundle 'fixtaglib'
+    }   
+  } // https://github.com/groovydev/twitter-bootstrap-grails-plugin/issues/32
 }
