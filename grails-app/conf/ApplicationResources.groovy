5421c2a5f5c660a645ee5f1fc154a7562152ca19
neodevelop <neodevelop@gmail.com>
2012-06-07 23:51:21 -0500

    Agregando recursos de JS

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,9 +2,15 @@ modules = {
   home {
     resource url:'/css/grain.css'
   }
+  payment {
+    resource url:'/js/payment/index.js'
+  }
   overrides { // Arreglando bootstra-fixtaglib.css
     'bootstrap-fixtaglib' {
       defaultBundle 'fixtaglib'
     }   
   } // https://github.com/groovydev/twitter-bootstrap-grails-plugin/issues/32
+  images {
+    resource url:'/images/spinner.gif'
+  }
 }
