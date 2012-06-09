a39085073d9751892bceadd2e61f5cbaa7d1b77d
neodevelop <neodevelop@gmail.com>
2012-06-08 23:44:19 -0500

    Agregando imágenes en resources

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -12,5 +12,11 @@ modules = {
   } // https://github.com/groovydev/twitter-bootstrap-grails-plugin/issues/32
   images {
     resource url:'/images/spinner.gif'
+    resource url:'/images/7-eleven-icon.png'
+    resource url:'/images/amex-icon.png'
+    resource url:'/images/banorte-icon.png'
+    resource url:'/images/mastercard-icon.gif'
+    resource url:'/images/oxxo-icon.gif'
+    resource url:'/images/visa-icon.gif'
   }
 }
