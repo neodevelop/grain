d4efa24d90a7541ceace017da7263019d0fdb0df
neodevelop <neodevelop@gmail.com>
2012-06-26 00:09:11 -0500

    Agregando valores al enum para usarlos como code en la vista

--- src/groovy/com/synergyj/grain/course/RegistrationStatus.groovy
+++ src/groovy/com/synergyj/grain/course/RegistrationStatus.groovy
@@ -16,11 +16,19 @@
 package com.synergyj.grain.course
 
 public enum RegistrationStatus {
-  REGISTERED,
-  INSCRIBED_AND_WITH_DEBTH,
-  INSCRIBED_AND_WITH_DEBTH_IN_GROUP,
-  INSCRIBED_AND_PAYED,
-  INSCRIBED_AND_PAYED_IN_GROUP,
-  FINISHED,
-  CANCELLED
+  REGISTERED("registration.registered.no.group"),
+  INSCRIBED_AND_WITH_DEBTH("registration.inscribed.with.debt"),
+  INSCRIBED_AND_WITH_DEBTH_IN_GROUP("registration.inscribed.with.debt.in.group"),
+  INSCRIBED_AND_PAYED("registration.inscribed.payed"),
+  INSCRIBED_AND_PAYED_IN_GROUP("registration.inscribed.payed.in.group"),
+  FINISHED("registration.finished"),
+  CANCELLED("registration.cancelled")
+
+  private final String code
+
+  RegistrationStatus(String code){
+    this.code = code
+  }
+
+  public String getCode(){ return this.code }
 }
