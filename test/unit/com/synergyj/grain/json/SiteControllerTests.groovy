01802b904117493c31b11ca52041acc871ea1905
jkings <jmanuel.reyes@synergyj.com>
2012-11-01 12:06:22 -0600

    Se agrega el test del controller para obtener los cursos agendados

---
+++ test/unit/com/synergyj/grain/json/SiteControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.synergyj.grain.json
+
+import grails.test.*
+
+class SiteControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
